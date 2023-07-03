885a9620e660bfbd085c47f9593b783d73c49537
mood173 <alswns1594@naver.com>
2023-07-03 17:27:41 +0900

    CreatureData 스탯 조작 함수 제작
      CreatureData
    스탯 제어 함수제작

--- Assets/Scripts/Creature/CreatureData.cs
+++ Assets/Scripts/Creature/CreatureData.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using TeamOdd.Ratocalypse.StatusEffectn;
 using UnityEngine;
 
-namespace TeamOdd.Ratocalypse.Object
+namespace TeamOdd.Ratocalypse.Creature
 {
     public class CreatureData : MonoBehaviour
     {
         [SerializeField] private float hp;
         [SerializeField] private int stamina;
-        [SerializeField] private List<StatusEffect> onStatusEffect = new List<StatusEffect>();
+        public List<StatusEffect> OnStatusEffect = new List<StatusEffect>();
+        public void HpControal(float value)
+        {
+            hp += value;
+        }
+        public void Stemina(int value)
+        {
+            if (stamina > value)
+            {
+                return;
+            }
+            stamina -= value;
+            //Card.Action 함수 들어갈 곳
+        }
+        
     }
 }
