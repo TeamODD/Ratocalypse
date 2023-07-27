bda2722e69ffb3a5aec18fffad865bf269729408
mood173 <gangm2832@gmial.com>
2023-07-27 20:34:06 +0900

    UI 명칭 통일, DeckData를 정수로 변경
    1. DeckData를 DeckDataCount로
       데이터 타입 정수로 변경
       UI 명칭 통일

--- Assets/Scripts/UI/DeckUi.cs
+++ Assets/Scripts/UI/DeckUi.cs
@@ -1,17 +1,16 @@
-using TeamOdd.Ratocalypse.Card;
 using TMPro;
 using UnityEngine;
 
-namespace TeamOdd.Ratocalypse.Ui
+namespace TeamOdd.Ratocalypse.UI
 {
-    public class DeckUi : MonoBehaviour
-    {   
-        public DeckData DeckData { get; private set; }
+    public class DeckUI : MonoBehaviour
+    {
+        public int DeckDataCount { get; private set; }
         public TextMeshProUGUI DeckText;
 
         public void Execute()
         {
-            DeckText.text = DeckData.Count.ToString();
+            DeckText.text = DeckDataCount.ToString();
         }
     }
 }
