d6801012762745aa268ac0a6c2382524446e7946
mood173 <gangm2832@gmial.com>
2023-07-31 21:47:51 +0900

    비활성화 변수와 기능 추가
    1. [추가점]
      각 UI마다 비활성화 변수와 함수 추가
      awake로 Text UI 받아오는 기능 추가

--- Assets/Scripts/UI/DeckUI.cs
+++ Assets/Scripts/UI/DeckUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace TeamOdd.Ratocalypse.UI
@@ -8,11 +9,22 @@ namespace TeamOdd.Ratocalypse.UI
     public class DeckUI : MonoBehaviour
     {
         public int DeckDataCount { get; private set; }
-        public TextMeshProUGUI DeckText;
+        public TextMeshProUGUI DeckText { get; set; }
+        public bool ActivationStatus { get; set; }
+
+        public void Awake()
+        {
+            DeckText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        }
 
         public void Execute()
         {
             DeckText.text = DeckDataCount.ToString();
+        }
+
+        public void Activation() 
+        {
+            gameObject.SetActive(ActivationStatus);
         }
     }
 }
--- Assets/Scripts/UI/TombUI.cs
+++ Assets/Scripts/UI/TombUI.cs
@@ -7,13 +7,24 @@ using UnityEngine;
 namespace TeamOdd.Ratocalypse.UI
 {
     public class TombUI : MonoBehaviour
-    {
+    { 
         public int TombDataCount { get; private set; }
-        public TextMeshProUGUI TombText;
+        public TextMeshProUGUI TombText { get; set; }
+        public bool ActivationStatus { get; set; }
+
+        public void Awake()
+        {
+            TombText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        }
 
         public void Execute()
         {
             TombText.text = TombDataCount.ToString();
+        }
+
+        public void Activation()
+        {
+            gameObject.SetActive(ActivationStatus);
         }
     }
 }
