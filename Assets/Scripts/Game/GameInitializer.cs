e10d6300104257382c6f77c3e198b6287da86839
110V <sgb1616@gmail.com>
2023-07-31 20:18:45 +0900

    GameInitializer 구현

---
+++ Assets/Scripts/Game/GameInitializer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TeamOdd.Ratocalypse.TestScripts;
+using UnityEngine;
+using static TeamOdd.Ratocalypse.MapLib.MapData;
+
+namespace TeamOdd.Ratocalypse.MapLib.GameLib
+{
+    public class GameInitializer:MonoBehaviour
+    {
+        private GameSequence _gameSequence;
+        private CommandExecutor _commandExecutor;
+
+        private GameStatistics _gameStatistics;
+
+        [SerializeField]
+        private TileSelector _tileSelector;
+        [SerializeField]
+        private Map _map;
+
+        private void Awake() 
+        {
+            Debug.Log("GameInitializer Awake");
+            _commandExecutor = new CommandExecutor(_map.MapData,_gameStatistics,_tileSelector, _tileSelector);
+            _gameSequence = new GameSequence(_map.MapData, _commandExecutor);
+        }
+        private void Start()
+        {
+            _gameSequence.Start();
+        }
+    }
+}
