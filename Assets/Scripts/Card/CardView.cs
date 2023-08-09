24d1cedfec7c98dc58bd442603393cec521152ed
110V <sgb1616@gmail.com>
2023-08-09 15:06:01 +0900

    카드 일러스트 추가 및 이펙트 개선

--- Assets/Scripts/Card/CardView.cs
+++ Assets/Scripts/Card/CardView.cs
@@ -2,18 +2,30 @@ using System;
 using UnityEngine;
 using UnityEngine.Events;
 using DG.Tweening;
+using System.Collections.Generic;
 
 namespace TeamOdd.Ratocalypse.CardLib
 {
     public class CardView : MonoBehaviour
     {
+        [SerializeField]
+        private Renderer _cardImage;
 
-        private CardData _cardData;
+        [SerializeField]
+        private Renderer _cardBackground;
+        [SerializeField]
+        private List<Texture2D> _cardBackgrounds;
 
-
-        public void Initialize(CardData cardData)
+        
+        public void View(CardData cardData)
         {
-            _cardData = cardData;
+            _cardImage.material.SetTexture("_Texture", cardData.Texture);
+            SetBackground(cardData.CardType);
+        }
+
+        public void SetBackground(int index)
+        {
+            _cardBackground.material.SetTexture("_Texture", _cardBackgrounds[index]);
         }
     }
 }
