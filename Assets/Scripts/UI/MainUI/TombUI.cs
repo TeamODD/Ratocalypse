2c1b5e045a9681cf1b0a5e1c02ae0e386c17dbba
110V <36877562+110V@users.noreply.github.com>
2023-08-09 21:29:53 +0900

    Revert "Revert "Feature/46 deck UI" (#55)"
    This reverts commit 38110ca7fe166ce30e980de651f1ac8fcdc913c5.

---
+++ Assets/Scripts/UI/MainUI/TombUI.cs
@@ -0,0 +1,27 @@
+using TMPro;
+using UnityEngine;
+
+
+namespace TeamOdd.Ratocalypse.UI
+{
+    public class TombUI : MonoBehaviour
+    { 
+        private TextMeshProUGUI _tombText;
+
+        public int TombDataCount { get; private set; }
+        public void Awake()
+        {
+            _tombText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        }
+
+        public void Execute()
+        {
+            _tombText.text = TombDataCount.ToString();
+        }
+
+        public void ActivationChange()
+        {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
+    }
+}
