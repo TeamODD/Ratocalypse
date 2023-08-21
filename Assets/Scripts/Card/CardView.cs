9a932a4be14d23e0a6741eacb59032b822dc754b
110V <sgb1616@gmail.com>
2023-08-21 20:28:10 +0900

    카드 타이틀 및 설명 임시 제작

--- Assets/Scripts/Card/CardView.cs
+++ Assets/Scripts/Card/CardView.cs
@@ -4,6 +4,8 @@ using UnityEngine.Events;
 using DG.Tweening;
 using System.Collections.Generic;
 using TeamOdd.Ratocalypse.DeckLib;
+using TMPro;
+
 
 namespace TeamOdd.Ratocalypse.CardLib
 {
@@ -14,19 +16,37 @@ namespace TeamOdd.Ratocalypse.CardLib
 
         [SerializeField]
         private Renderer _cardBackground;
+
         [SerializeField]
         private List<Texture2D> _cardBackgrounds;
 
+        [SerializeField]
+        private TextMeshPro _title;
+
+        [SerializeField]
+        private TextMeshPro _description;
         
-        public void View(CardData cardData,CardColor cardType)
+        public void View(CardData cardData, CardColor cardType)
         {
             _cardImage.material.SetTexture("_Texture", cardData.Texture);
             SetBackground((int)cardType);
+            SetTitle(cardData.GetTitle());
+            SetDescription(cardData.GetDescription());
         }
 
         public void SetBackground(int index)
         {
             _cardBackground.material.SetTexture("_Texture", _cardBackgrounds[index]);
+        }
+
+        public void SetTitle(string title)
+        {
+            _title.text = title;
+        }
+
+        public void SetDescription(string description)
+        {
+            _description.text = description;
         }
     }
 }
