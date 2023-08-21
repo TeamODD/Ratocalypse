d610619f754e2117e662a7ef2861d233949781d6
mood173 <gangm2832@gmial.com>
2023-08-21 17:11:41 +0900

    UI Render 모드 변경 그에 따른  크리쳐 인터페이 인터페이스 오류 수정
    1. [변경 & 수정]
       이펙트 효과가 UI위에 그려 넣기 위해 UIRender모드 수정
       HP바가 크리쳐 머리위에서 이동하지 못하는문제 해결
    
    2. [영향]
    
    3. [테스트 방법]

--- Assets/Scripts/UI/MainUI/CreatureUI.cs
+++ Assets/Scripts/UI/MainUI/CreatureUI.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
 
 public class CreatureUI : MonoBehaviour
 {
     // 테스트용 임시 변수
     public Transform target;
+    public RectTransform rectTransform;
 
-    void Start()
+    private Vector3 InterfacePoint;  
+    public void Start()
     {
-
+        rectTransform = transform.GetComponent<RectTransform>();
     }
-
-    void LateUpdate()
+    public void LateUpdate()
     {
-        transform.position = Camera.main.WorldToScreenPoint(target.position) + new Vector3(0, 150, 0);
+        // transform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, target.position);
+        //transform.position = Camera.main.WorldToScreenPoint(target.position + new Vector3(0, 150, 0)) ;
+         InterfacePoint = RectTransformUtility.WorldToScreenPoint(Camera.main, target.position);
+        rectTransform.anchoredPosition = InterfacePoint + new Vector3(0, 150, 0);
     }
 }
