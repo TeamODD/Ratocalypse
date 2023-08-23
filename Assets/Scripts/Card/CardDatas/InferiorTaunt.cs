60e7fe3d1caca4aace5f2f95181cf62d0c032d24
110V <sgb1616@gmail.com>
2023-08-23 21:23:23 +0900

    질 낮은 도발 구현

---
+++ Assets/Scripts/Card/CardDatas/InferiorTaunt.cs
@@ -0,0 +1,94 @@
+using TeamOdd.Ratocalypse.CardLib.CommandLib;
+using TeamOdd.Ratocalypse.CreatureLib;
+using TeamOdd.Ratocalypse.CreatureLib.Attributes;
+using TeamOdd.Ratocalypse.DeckLib;
+using TeamOdd.Ratocalypse.MapLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.CardCommands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.ActionCommands;
+using UnityEngine;
+using System.Collections.Generic;
+using static TeamOdd.Ratocalypse.MapLib.MapData;
+using TeamOdd.Ratocalypse.MapLib.GameLib.MovemnetLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib;
+using TeamOdd.Ratocalypse.CreatureLib.Cat;
+
+namespace TeamOdd.Ratocalypse.CardLib.CardDatas.Templates
+{
+    [CardRegister(typeof(ValueData))]
+    public class InferiorTaunt : CardData
+    {
+
+        public override string GetTitle()
+        {
+            return "질 낮은 도발";
+        }
+
+        public override string GetDescription()
+        {
+            return $"적을 이번 라운드 동안 도발합니다.\n방어도를 {GetAmount()} 얻습니다.";
+        }
+
+        private int GetAmount()
+        {
+            var originValueData = OriginValueData as ValueData;
+            var gameValueData = GameValueData as ValueData;
+            return originValueData.Amount + gameValueData.Amount; 
+        }
+
+        private DirectionalMovement CreateMovement(CreatureData caster)
+        {
+            Pattern pattern = Pattern.GetChessPattern(OriginValueData.rangeType);
+            DirectionalMovement movement = new DirectionalMovement(caster, pattern);
+            return movement;
+        }
+
+        public override MapSelecting GetPreview(CreatureData caster)
+        {
+            return CreateMovement(caster);
+        }
+
+        public override CastCard CreateCastCardCommand(CardCastData cardCastData, bool runTrigger)
+        {
+            CreatureData caster = null;
+            CastCard castCard = new CastCard(cardCastData, runTrigger);
+
+            castCard.AddCommand((result) =>
+            {
+                CardCastData data = result as CardCastData;
+                caster = data.Caster;
+                var temp = new SelectMap(CreateMovement(caster), data.Caster, false, true);
+                return temp;
+            });
+
+
+            castCard.SetTrigger((result, _) =>
+            {
+                int amount = GetAmount();
+                SelectMap.Result selectResult = result as SelectMap.Result;
+
+                TriggerCard triggerCard = new TriggerCard(null, caster, 0, selectResult.SelectedCoord);
+                triggerCard.AddCommand((_) =>
+                {
+                    return new GainArmor(caster, amount);
+                });
+                if (selectResult.SelectedCoord != null)
+                {
+                    triggerCard.AddCommand((_) =>
+                    {
+                        return new Move(caster, selectResult.SelectedCoord.Value);
+                    });
+                }
+
+                return triggerCard;
+            });
+
+            return castCard;
+        }
+
+        public class ValueData : CardValueData
+        {
+            public int Amount = 0;
+        }
+    }
+}
