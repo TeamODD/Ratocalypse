86dee7f5ed2ea1b50743080d31a050742e4522d0
110V <sgb1616@gmail.com>
2023-08-24 02:48:08 +0900

    즐거웠던 그 시절 구현

---
+++ Assets/Scripts/Card/CardDatas/AttackCards/JoyfulDays.cs
@@ -0,0 +1,106 @@
+using TeamOdd.Ratocalypse.CardLib.CommandLib;
+using TeamOdd.Ratocalypse.CreatureLib;
+using TeamOdd.Ratocalypse.CreatureLib.Attributes;
+using TeamOdd.Ratocalypse.DeckLib;
+using TeamOdd.Ratocalypse.MapLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.CardCommands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.ActionCommands;
+using UnityEngine;
+using System.Collections.Generic;
+using static TeamOdd.Ratocalypse.MapLib.MapData;
+using TeamOdd.Ratocalypse.MapLib.GameLib.MovemnetLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib;
+using TeamOdd.Ratocalypse.CreatureLib.Cat;
+
+namespace TeamOdd.Ratocalypse.CardLib.CardDatas.Templates
+{
+    [CardRegister(typeof(ValueData))]
+    public class JoyfulDays : CardData
+    {
+
+        public override string GetTitle()
+        {
+            return "즐거웠던 그 시절";
+        }
+
+        public override string GetDescription()
+        {
+            return $"피해를 {GetDamage()}줍니다.\n이후 최대 스테미나를 {GetAmount()}만큼 증가시킵니다.";
+        }
+
+        private int GetAmount()
+        {
+            var originValueData = OriginValueData as ValueData;
+            var gameValueData = GameValueData as ValueData;
+            return originValueData.Amount + gameValueData.Amount;
+        }
+        private int GetDamage()
+        {
+            var originValueData = OriginValueData as ValueData;
+            var gameValueData = GameValueData as ValueData;
+            return originValueData.Damage + gameValueData.Damage;
+        }
+
+        private DirectionalMovement CreateMovement(CreatureData caster)
+        {
+            Pattern pattern = Pattern.GetChessPattern(OriginValueData.rangeType);
+            DirectionalMovement movement = new DirectionalMovement(caster, pattern);
+            return movement;
+        }
+
+        public override MapSelecting GetPreview(CreatureData caster)
+        {
+            return CreateMovement(caster);
+        }
+
+        public override CastCard CreateCastCardCommand(CardCastData cardCastData, bool runTrigger)
+        {
+            CreatureData caster = null;
+            CastCard castCard = new CastCard(cardCastData, runTrigger);
+
+            castCard.AddCommand((result) =>
+            {
+                CardCastData data = result as CardCastData;
+                caster = data.Caster;
+                var temp = new SelectMap(CreateMovement(caster), data.Caster, true, true);
+                return temp;
+            });
+
+            int damage = GetDamage();
+            int amount = GetAmount();
+            castCard.SetTrigger((result, _) =>
+            {
+                SelectMap.Result selectResult = result as SelectMap.Result;
+
+                TriggerCard triggerCard = new TriggerCard(null, caster, damage, selectResult.SelectedCoord);
+                triggerCard.AddCommand((_) =>
+                {
+                    if (selectResult.SelectedCoord != null)
+                    {
+                        return new Move(caster, selectResult.SelectedCoord.Value);
+                    }
+                    if (selectResult.SelectedPlacement != null)
+                    {
+                        IDamageable target = selectResult.SelectedPlacement as IDamageable;
+                        return new Attack(caster, target, triggerCard.CalculateFinalDamage());
+                    }
+                    return null;
+                });
+                triggerCard.AddCommand((_)=>{
+                    return new IncreaseMaxStamina(caster, amount);
+                });
+
+                return triggerCard;
+            });
+
+            return castCard;
+        }
+
+        public class ValueData : CardValueData
+        {
+            public int Amount = 0;
+            public int Damage = 0;
+        }
+    }
+}
