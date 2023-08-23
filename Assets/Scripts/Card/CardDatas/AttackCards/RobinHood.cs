b68fd9f673764a1ceb837b66c0e30caa12d936bd
110V <sgb1616@gmail.com>
2023-08-23 21:56:43 +0900

    로빈 후드 구현

---
+++ Assets/Scripts/Card/CardDatas/AttackCards/RobinHood.cs
@@ -0,0 +1,104 @@
+using TeamOdd.Ratocalypse.CardLib.CommandLib;
+using TeamOdd.Ratocalypse.CreatureLib;
+using TeamOdd.Ratocalypse.CreatureLib.Attributes;
+using TeamOdd.Ratocalypse.DeckLib;
+using TeamOdd.Ratocalypse.MapLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.CardCommands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.ActionCommands;
+using UnityEngine;
+using System.Collections.Generic;
+using static TeamOdd.Ratocalypse.MapLib.MapData;
+using TeamOdd.Ratocalypse.MapLib.GameLib.MovemnetLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib;
+using TeamOdd.Ratocalypse.CreatureLib.Cat;
+
+namespace TeamOdd.Ratocalypse.CardLib.CardDatas.Templates
+{
+    [CardRegister(typeof(ValueData))]
+    public class RobinHood : CardData
+    {
+
+        public override string GetTitle()
+        {
+            return "로빈 후드";
+        }
+
+        public override string GetDescription()
+        {
+            return $"피해를 ({GetDamage()} + 적과 캐릭터의 거리)만큼 줍니다.";
+        }
+
+        private int GetDamage()
+        {
+            var originValueData = OriginValueData as ValueData;
+            var gameValueData = GameValueData as ValueData;
+            return originValueData.Damage + gameValueData.Damage; 
+        }
+
+        private DirectionalMovement CreateMovement(CreatureData caster)
+        {
+            Pattern pattern = Pattern.GetChessPattern(OriginValueData.rangeType);
+            DirectionalMovement movement = new DirectionalMovement(caster, pattern, (placement)=>{
+                return Utils.IsEnemy(caster, placement);
+            });
+            return movement;
+        }
+
+        public override MapSelecting GetPreview(CreatureData caster)
+        {
+            return CreateMovement(caster);
+        }
+
+        public override CastCard CreateCastCardCommand(CardCastData cardCastData, bool runTrigger)
+        {
+            CreatureData caster = null;
+            CastCard castCard = new CastCard(cardCastData, runTrigger);
+
+            castCard.AddCommand((result) =>
+            {
+                CardCastData data = result as CardCastData;
+                caster = data.Caster;
+                var temp = new SelectMap(CreateMovement(caster), data.Caster, true, true);
+                return temp;
+            });
+
+            int damage = GetDamage();
+
+            castCard.SetTrigger((result, _) =>
+            {
+                SelectMap.Result selectResult = result as SelectMap.Result;
+                if (selectResult.SelectedPlacement != null)
+                {
+                    Placement target = selectResult.SelectedPlacement;
+                    int distance = MapAnalyzer.GetDistance(caster, target);
+                    damage += distance;
+                }
+                
+                TriggerCard triggerCard = new TriggerCard(null, caster, damage, selectResult.SelectedCoord);
+                triggerCard.AddCommand((_) =>
+                {
+                    if (selectResult.SelectedCoord != null)
+                    {
+                        return new Move(caster, selectResult.SelectedCoord.Value);
+                    }
+                    if (selectResult.SelectedPlacement != null)
+                    {
+                        IDamageable target = selectResult.SelectedPlacement as IDamageable;
+                        return new Attack(caster, target, triggerCard.CalculateFinalDamage());
+                    }
+                    return null;
+                });
+
+                return triggerCard;
+            });
+
+            return castCard;
+        }
+
+        public class ValueData : CardValueData
+        {
+            public int Damage = 0;
+        }
+    }
+}
