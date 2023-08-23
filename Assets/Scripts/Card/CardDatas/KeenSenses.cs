fb856e031fea757833b44776507391841ab29dc4
110V <sgb1616@gmail.com>
2023-08-24 02:47:53 +0900

    예리한 감각 구현

---
+++ Assets/Scripts/Card/CardDatas/KeenSenses.cs
@@ -0,0 +1,93 @@
+using TeamOdd.Ratocalypse.CardLib.CommandLib;
+using TeamOdd.Ratocalypse.CreatureLib;
+using TeamOdd.Ratocalypse.CreatureLib.Attributes;
+using TeamOdd.Ratocalypse.DeckLib;
+using TeamOdd.Ratocalypse.MapLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.CardCommands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.ActionCommands;
+using UnityEngine;
+using System.Collections.Generic;
+using static TeamOdd.Ratocalypse.MapLib.MapData;
+using TeamOdd.Ratocalypse.MapLib.GameLib.MovemnetLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib;
+using TeamOdd.Ratocalypse.CreatureLib.Cat;
+
+namespace TeamOdd.Ratocalypse.CardLib.CardDatas.Templates
+{
+    [CardRegister(typeof(ValueData))]
+    public class KeenSenses : CardData
+    {
+
+        public override string GetTitle()
+        {
+            return "예리한 감각";
+        }
+
+        public override string GetDescription()
+        {
+            return $"이동 후: 카드를 {GetCount()}장 뽑습니다.";
+        }
+
+        private int GetCount()
+        {
+            var originValueData = OriginValueData as ValueData;
+            var gameValueData = GameValueData as ValueData;
+            return originValueData.Count + gameValueData.Count; 
+        }
+
+        private DirectionalMovement CreateMovement(CreatureData caster)
+        {
+            Pattern pattern = Pattern.GetChessPattern(OriginValueData.rangeType);
+            DirectionalMovement movement = new DirectionalMovement(caster, pattern);
+            return movement;
+        }
+
+        public override MapSelecting GetPreview(CreatureData caster)
+        {
+            return CreateMovement(caster);
+        }
+
+        public override CastCard CreateCastCardCommand(CardCastData cardCastData, bool runTrigger)
+        {
+            CreatureData caster = null;
+            CastCard castCard = new CastCard(cardCastData, runTrigger);
+
+            castCard.AddCommand((result) =>
+            {
+                CardCastData data = result as CardCastData;
+                caster = data.Caster;
+                var temp = new SelectMap(CreateMovement(caster), data.Caster, false, true);
+                return temp;
+            });
+
+            int count = GetCount();
+            castCard.SetTrigger((result, _) =>
+            {
+                SelectMap.Result selectResult = result as SelectMap.Result;
+
+                TriggerCard triggerCard = new TriggerCard(null, caster, 0, selectResult.SelectedCoord);
+                if (selectResult.SelectedCoord != null)
+                {
+                    triggerCard.AddCommand((_) =>
+                    {
+                        return new Move(caster, selectResult.SelectedCoord.Value);
+                    });
+                }
+                triggerCard.AddCommand((_) =>
+                {
+                    return new Draw(caster, count, false);
+                });
+
+                return triggerCard;
+            });
+
+            return castCard;
+        }
+
+        public class ValueData : CardValueData
+        {
+            public int Count = 0;
+        }
+    }
+}
