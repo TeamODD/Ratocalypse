8aa63d5bb26039616b85fbb29ade33b175dcfe5f
110V <sgb1616@gmail.com>
2023-08-25 05:16:28 +0900

    신벌이 있으리 구현

---
+++ Assets/Scripts/Card/CardDatas/AttackCards/Curse.cs
@@ -0,0 +1,90 @@
+using TeamOdd.Ratocalypse.CardLib.CommandLib;
+using TeamOdd.Ratocalypse.CreatureLib;
+using TeamOdd.Ratocalypse.CreatureLib.Attributes;
+using TeamOdd.Ratocalypse.DeckLib;
+using TeamOdd.Ratocalypse.MapLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.CardCommands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.ActionCommands;
+using UnityEngine;
+using System.Collections.Generic;
+using static TeamOdd.Ratocalypse.MapLib.MapData;
+using TeamOdd.Ratocalypse.MapLib.GameLib.MovemnetLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib;
+using TeamOdd.Ratocalypse.CreatureLib.Cat;
+
+namespace TeamOdd.Ratocalypse.CardLib.CardDatas.Templates
+{
+    [CardRegister(typeof(ValueData))]
+    public class Curse : CardData
+    {
+
+        public override string GetTitle()
+        {
+            return "신벌이 있으리";
+        }
+
+        public override string GetDescription()
+        {
+            return $"공격 시 피해 대신 적에게 '신벌' 카드를 한 장 추가합니다.";
+        }
+
+        private DirectionalMovement CreateMovement(CreatureData caster)
+        {
+            Pattern pattern = Pattern.GetChessPattern(OriginValueData.rangeType);
+            DirectionalMovement movement = new DirectionalMovement(caster, pattern, (placement)=>{
+                return Utils.IsEnemy(caster, placement);
+            });
+            return movement;
+        }
+
+        public override MapSelecting GetPreview(CreatureData caster)
+        {
+            return CreateMovement(caster);
+        }
+
+        public override CastCard CreateCastCardCommand(CardCastData cardCastData, bool runTrigger)
+        {
+            CreatureData caster = null;
+            CastCard castCard = new CastCard(cardCastData, runTrigger);
+
+            castCard.AddCommand((result) =>
+            {
+                CardCastData data = result as CardCastData;
+                caster = data.Caster;
+                var temp = new SelectMap(CreateMovement(caster), data.Caster, true, true);
+                return temp;
+            });
+
+            castCard.SetTrigger((result, _) =>
+            {
+                SelectMap.Result selectResult = result as SelectMap.Result;
+
+                TriggerCard triggerCard = new TriggerCard(null, caster, 0, selectResult.SelectedCoord);
+                triggerCard.AddCommand((_) =>
+                {
+                    if (selectResult.SelectedCoord != null)
+                    {
+                        return new Move(caster, selectResult.SelectedCoord.Value);
+                    }
+                    if (selectResult.SelectedPlacement != null)
+                    {
+                        CreatureData target = selectResult.SelectedPlacement as CreatureData;
+                        var cardId = (result as ValueData).CardId;
+                        return new InsertCard(target, cardId);
+                    }
+                    return null;
+                });
+
+                return triggerCard;
+            });
+
+            return castCard;
+        }
+
+        public class ValueData : CardValueData
+        {
+            public int CardId = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Card/CardDatas/AttackCards/Cursed.cs
@@ -0,0 +1,56 @@
+using TeamOdd.Ratocalypse.CardLib.CommandLib;
+using TeamOdd.Ratocalypse.CreatureLib;
+using TeamOdd.Ratocalypse.CreatureLib.Attributes;
+using TeamOdd.Ratocalypse.DeckLib;
+using TeamOdd.Ratocalypse.MapLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.CardCommands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.ActionCommands;
+using UnityEngine;
+using System.Collections.Generic;
+using static TeamOdd.Ratocalypse.MapLib.MapData;
+using TeamOdd.Ratocalypse.MapLib.GameLib.MovemnetLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib;
+using TeamOdd.Ratocalypse.CreatureLib.Cat;
+
+namespace TeamOdd.Ratocalypse.CardLib.CardDatas.Templates
+{
+    [CardRegister(typeof(ValueData))]
+    public class Cursed : CardData, IVolatileCard
+    {
+
+        public override string GetTitle()
+        {
+            return "저주 : 신벌";
+        }
+
+        public override string GetDescription()
+        {
+            return $"효과가 없습니다.\n사용하거나 다음 라운드가 되면 사라집니다.";
+        }
+
+        public override MapSelecting GetPreview(CreatureData caster)
+        {
+            return null;
+        }
+
+        public override CastCard CreateCastCardCommand(CardCastData cardCastData, bool runTrigger)
+        {
+            CreatureData caster = null;
+            CastCard castCard = new CastCard(cardCastData, runTrigger);
+
+            castCard.SetTrigger((_, _) =>
+            {
+                TriggerCard triggerCard = new TriggerCard(null, caster, 0, null);
+                return triggerCard;
+            });
+
+            return castCard;
+        }
+
+        public class ValueData : CardValueData
+        {
+
+        }
+    }
+}
