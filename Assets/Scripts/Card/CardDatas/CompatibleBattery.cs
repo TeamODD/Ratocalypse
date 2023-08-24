e8936a56771d038db71ffdc6643b5dd5d31c1975
110V <sgb1616@gmail.com>
2023-08-25 03:16:58 +0900

    호환 배터리 구현

---
+++ Assets/Scripts/Card/CardDatas/CompatibleBattery.cs
@@ -0,0 +1,96 @@
+using TeamOdd.Ratocalypse.CardLib.CommandLib;
+using TeamOdd.Ratocalypse.CreatureLib;
+using TeamOdd.Ratocalypse.CreatureLib.Attributes;
+using TeamOdd.Ratocalypse.DeckLib;
+using TeamOdd.Ratocalypse.MapLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.CardCommands;
+using TeamOdd.Ratocalypse.MapLib.GameLib.Commands.ActionCommands;
+using UnityEngine;
+using System.Collections.Generic;
+using static TeamOdd.Ratocalypse.MapLib.MapData;
+using TeamOdd.Ratocalypse.MapLib.GameLib.MovemnetLib;
+using TeamOdd.Ratocalypse.MapLib.GameLib;
+using TeamOdd.Ratocalypse.CreatureLib.Cat;
+
+namespace TeamOdd.Ratocalypse.CardLib.CardDatas.Templates
+{
+    [CardRegister(typeof(ValueData))]
+    public class CompatibleBattery : CardData
+    {
+
+        public override string GetTitle()
+        {
+            return "호환 배터리";
+        }
+
+        public override string GetDescription()
+        {
+            return $"이동 후: 카드를 사용한 후 남은 스테미나 수 만큼 카드를 뽑습니다.";
+        }
+
+        private int GetDamage()
+        {
+            var originValueData = OriginValueData as ValueData;
+            var gameValueData = GameValueData as ValueData;
+            return originValueData.Damage + gameValueData.Damage;
+        }
+
+        private DirectionalMovement CreateMovement(CreatureData caster)
+        {
+            Pattern pattern = Pattern.GetChessPattern(OriginValueData.rangeType);
+            DirectionalMovement movement = new DirectionalMovement(caster, pattern, (placement) =>
+            {
+                return Utils.IsEnemy(caster, placement);
+            });
+            return movement;
+        }
+
+        public override MapSelecting GetPreview(CreatureData caster)
+        {
+            return CreateMovement(caster);
+        }
+
+        public override CastCard CreateCastCardCommand(CardCastData cardCastData, bool runTrigger)
+        {
+            CreatureData caster = null;
+            CastCard castCard = new CastCard(cardCastData, runTrigger);
+
+            castCard.AddCommand((result) =>
+            {
+                CardCastData data = result as CardCastData;
+                caster = data.Caster;
+                var temp = new SelectMap(CreateMovement(caster), data.Caster, true, true);
+                return temp;
+            });
+
+            castCard.SetTrigger((result, _) =>
+            {
+                int count = caster.Stamina;
+                SelectMap.Result selectResult = result as SelectMap.Result;
+                TriggerCard triggerCard = new TriggerCard(null, caster, 0, selectResult.SelectedCoord);
+
+                if (selectResult.SelectedCoord != null)
+                {
+                    triggerCard.AddCommand((_) =>
+                    {
+                        return new Move(caster, selectResult.SelectedCoord.Value);
+                    });
+                }
+                triggerCard.AddCommand((_) =>
+                {
+                    return new Draw(caster, count, false);
+                });
+
+                return triggerCard;
+            });
+
+            return castCard;
+        }
+
+        public class ValueData : CardValueData
+        {
+            public int Damage = 0;
+        }
+    }
+}
