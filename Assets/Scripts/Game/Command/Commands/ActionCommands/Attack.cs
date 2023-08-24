29ca76d486887bf7981a5567d6f4b1a5230f1674
110V <sgb1616@gmail.com>
2023-08-25 04:50:42 +0900

    다중 대상 공격 지원

--- Assets/Scripts/Game/Command/Commands/ActionCommands/Attack.cs
+++ Assets/Scripts/Game/Command/Commands/ActionCommands/Attack.cs
@@ -3,62 +3,87 @@ using TeamOdd.Ratocalypse.CreatureLib.Attributes;
 using TeamOdd.Ratocalypse.CardLib.CommandLib;
 using System;
 using TeamOdd.Ratocalypse.CreatureLib;
+using System.Collections.Generic;
 
 namespace TeamOdd.Ratocalypse.MapLib.GameLib.Commands.ActionCommands
 {
-    public class Attack : TargetCommand<IDamageable>, IRequireExtraExcutor
+    public class Attack : Command, IRequireExtraExcutor
     {
         private Func<CommandExecutor> _createExecutor;
         private int _damage = 0;
         private IAttackable _attacker;
-        private IDamageable _target;
+        private List<IDamageable> _targets;
 
         public void SetRequire(Func<CommandExecutor> require)
         {
             _createExecutor = require;
         }
 
-        public Attack(IAttackable attacker, IDamageable target, int damage) : base(target)
+        public Attack(IAttackable attacker, IDamageable target, int damage)
         {
             _damage = damage;
             _attacker = attacker;
-            _target = target;
+            _targets = new List<IDamageable>() { target };
         }
 
-        protected override ExecuteResult RunSuccess()
+        public Attack(IAttackable attacker, List<IDamageable> targets, int damage)
         {
-            if(_attacker is IDamageable attacker && attacker.IsAlive() == false)
+            _damage = damage;
+            _attacker = attacker;
+            _targets = targets;
+        }
+
+        public override ExecuteResult Execute()
+        {
+            if (_attacker is IDamageable attacker && attacker.IsAlive() == false)
             {
-                return new End(WrapResult(false));
+                return new End();
             }
 
             var (endWait, result) = CreateWait();
-            
             var attackExecutor = _createExecutor();
-            var damageExecutor = _createExecutor();
 
-            var count = 2;
+            var count = _targets.Count + 1;
+
+            List<CommandExecutor> damageExecutors = new List<CommandExecutor>();
+            foreach (var target in _targets)
+            {
+                var damageExecutor = _createExecutor();
+                damageExecutors.Add(damageExecutor);
+                damageExecutor.OnEmpty.AddListener(EndExecute);
+
+                var damage = new Damage(target, _damage);
+                damageExecutor.PushCommand(damage);
+            }
+
+            Animation animation = new Animation(_attacker, "Attack", true, null,
+            () =>
+            {
+                damageExecutors.ForEach(damageExecutor => damageExecutor.Run());
+            }, () => { });
+
+            attackExecutor.PushCommand(animation);
+            attackExecutor.Run();
+            attackExecutor.OnEmpty.AddListener(EndExecute);
+
 
             void EndExecute()
             {
                 count--;
                 if (count == 0)
                 {
-                    endWait(new End(WrapResult(true)));
+                    if (_attacker is CreatureData creatureData)
+                    {
+                        var (has, _) = creatureData.HasEffect("Mozzarella");
+                        if (has)
+                        {
+                            endWait(new NextCommand(new Heal(creatureData, _damage)));
+                            return;
+                        }
+                    }
+                    endWait(new End());
                 }
             }
-
-            Animation animation = new Animation(_attacker, "Attack", true, null,
-            () =>{
-                var damage = new Damage(_target, _damage);
-                damageExecutor.PushCommand(damage);
-                damageExecutor.Run();
-                damageExecutor.OnEmpty.AddListener(EndExecute);
-            }, ()=>{});
-
-            attackExecutor.PushCommand(animation);
-            attackExecutor.Run();
-            attackExecutor.OnEmpty.AddListener(EndExecute);
 
             return result;
         }
